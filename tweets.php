340cff20aec7368b51c358f4f025de49ed954261
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-06-15 17:29:27 -0300

    PHP archive to retrieve twitter timeline

---
+++ tweets.php
@@ -0,0 +1,32 @@
+<?
+//Twitter OAuth library: https://github.com/mynetx/codebird-php
+require_once ('codebird.php');
+
+//Twitter OAuth Settings:
+$CONSUMER_KEY = '';
+$CONSUMER_SECRET = '';
+$ACCESS_TOKEN = '';
+$ACCESS_TOKEN_SECRET = '';
+
+//Get authenticated:
+Codebird::setConsumerKey($CONSUMER_KEY, $CONSUMER_SECRET);
+$cb = Codebird::getInstance();
+$cb->setToken($ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+
+//Retrieve posts:
+$username = $_GET['username'];
+$count = $_GET['count'];
+$api = $_GET['api'];
+
+//API Settings: https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
+$params = array(
+	'screen_name' => $username,
+	'count' => $count
+);
+
+//Make the REST call:
+$data = (array) $cb->$api($params);
+
+//Output result in JSON:
+echo json_encode($data);
+?>
