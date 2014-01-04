62fd24abb4f38eb93a4d47dd09475a5fdccfa232
Pedro Rogério <pinceladasdaweb@hotmail.com>
2014-01-04 15:04:47 -0200

    Code improvements

--- tweets.php
+++ tweets.php
@@ -1,32 +1,39 @@
-<?
-//Twitter OAuth library: https://github.com/mynetx/codebird-php
-require_once ('codebird.php');
+<?php
+class Tweets {
+    static public function user_timeline() {
+        // Twitter OAuth library: https://github.com/mynetx/codebird-php
+        require_once ('codebird.php');
 
-//Twitter OAuth Settings:
-$CONSUMER_KEY = '';
-$CONSUMER_SECRET = '';
-$ACCESS_TOKEN = '';
-$ACCESS_TOKEN_SECRET = '';
+        // Twitter OAuth Settings:
+        $CONSUMER_KEY = '';
+        $CONSUMER_SECRET = '';
+        $ACCESS_TOKEN = '';
+        $ACCESS_TOKEN_SECRET = '';
 
-//Get authenticated:
-Codebird::setConsumerKey($CONSUMER_KEY, $CONSUMER_SECRET);
-$cb = Codebird::getInstance();
-$cb->setToken($ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+        // Get authenticated:
+        \Codebird\Codebird::setConsumerKey($CONSUMER_KEY, $CONSUMER_SECRET);
+        $cb = \Codebird\Codebird::getInstance();
+        $cb->setToken($ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
 
-//Retrieve posts:
-$username = $_GET['username'];
-$count = $_GET['count'];
-$api = $_GET['api'];
+        // Retrieve posts:
+        $username = strip_tags(trim($_GET['username']));
+        $count = strip_tags(trim($_GET['count']));
 
-//API Settings: https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
-$params = array(
-	'screen_name' => $username,
-	'count' => $count
-);
+        // API Settings: https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
+        $params = array(
+            'screen_name' => $username,
+            'count' => $count
+        );
 
-//Make the REST call:
-$data = (array) $cb->$api($params);
+        // Make the REST call:
+        $data = (array) $cb->statuses_userTimeline($params);
 
-//Output result in JSON:
-echo json_encode($data);
-?>
+        unset($data['httpstatus']);
+
+        // Output result in JSON:
+        return json_encode($data);
+    }
+}
+
+header('Content-type: application/json');
+echo Tweets::user_timeline();
